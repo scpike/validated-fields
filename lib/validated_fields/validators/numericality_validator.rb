cc2f2b137803cf839cdea11801521e3ab5b70d31
Stephen Pike <spike@scpike.net>
2011-02-02 00:21:09 -0500

    Modified numericality_validator.rb to take nonintegers and also to handle the :only_integer validation argument.

--- lib/validated_fields/validators/numericality_validator.rb
+++ lib/validated_fields/validators/numericality_validator.rb
@@ -7,13 +7,15 @@ module ValidatedFields
 
       def self.prepare_options(validator, options)
         voptions = validator.options
-        
+        precision = 0.000000001
         # FIXME: will work properly only for integers
-        options[:min] = voptions[:greater_than].to_i + 1 if voptions[:greater_than].present?
-        options[:max] = voptions[:less_than].to_i    - 1 if voptions[:less_than].present?
+        options[:min] = voptions[:greater_than].to_f + precision if voptions[:greater_than].present?
+        options[:max] = voptions[:less_than].to_f    - precision if voptions[:less_than].present?
         
-        options[:min] = voptions[:greater_than_or_equal_to].to_i + 1 if voptions[:greater_than_or_equal_to].present?
-        options[:max] = voptions[:less_than_or_equal_to].to_i    - 1 if voptions[:less_than_or_equal_to].present?
+        options[:min] = voptions[:greater_than_or_equal_to].to_f if voptions[:greater_than_or_equal_to].present?
+        options[:max] = voptions[:less_than_or_equal_to].to_f  if voptions[:less_than_or_equal_to].present?
+        
+        options[:only_integer] = voptions[:only_integer]
         
         options['data-error-numericality'] = voptions[:message] || "Invalid number"
         options
