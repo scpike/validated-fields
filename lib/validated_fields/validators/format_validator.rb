ab60f42ceb9a6629904bf5f13a7e7b0757d8b087
Stephen Pike <spike@scpike.net>
2011-02-05 22:06:50 -0500

    Changing pattern name

--- lib/validated_fields/validators/format_validator.rb
+++ lib/validated_fields/validators/format_validator.rb
@@ -8,7 +8,7 @@ module ValidatedFields
       def self.prepare_options(validator, options)
         voptions = validator.options
         # TODO: tidy up regexes for javascript compatibility
-        options[:pattern] = voptions[:with].inspect
+        options[:format_pattern] = voptions[:with].inspect
 
         options["data-allow-blank"]  = true if voptions[:allow_nil].present?   && voptions[:allow_nil]   == true
         options["data-allow-blank"]  = true if voptions[:allow_blank].present? && voptions[:allow_blank] == true
